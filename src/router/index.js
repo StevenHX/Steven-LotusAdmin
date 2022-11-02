5130a2537d5b973aa5884dd709b3bab8269dc532
wingtech\huanxiao <18122147149@qq.com>
2022-11-02 10:07:02 +0800

    add user&profile page

--- src/router/index.js
+++ src/router/index.js
@@ -96,16 +96,16 @@ export const constantRoutes = [
 // 动态路由，基于用户权限动态去加载
 export const dynamicRoutes = [
   {
-    path: '/system/dict-data',
+    path: '/system/user-auth',
     component: Layout,
     hidden: true,
-    permissions: ['system:dict:list'],
+    permissions: ['system:user:edit'],
     children: [
       {
-        path: 'index/:dictId(\\d+)',
-        component: () => import('@/views/system/dict/data'),
-        name: 'Data',
-        meta: { title: '字典数据', activeMenu: '/system/dict' }
+        path: 'role/:userId(\\d+)',
+        component: () => import('@/views/system/user/authRole'),
+        name: 'AuthRole',
+        meta: { title: '分配角色', activeMenu: '/system/user' }
       }
     ]
   },
@@ -123,6 +123,20 @@ export const dynamicRoutes = [
       }
     ]
   },
+  {
+    path: '/system/dict-data',
+    component: Layout,
+    hidden: true,
+    permissions: ['system:dict:list'],
+    children: [
+      {
+        path: 'index/:dictId(\\d+)',
+        component: () => import('@/views/system/dict/data'),
+        name: 'Data',
+        meta: { title: '字典数据', activeMenu: '/system/dict' }
+      }
+    ]
+  }
 ]
 
 const router = createRouter({
--- src/utils/common.js
+++ src/utils/common.js
@@ -167,3 +167,11 @@ export function useDict(...args) {
   }
   return tree;
 }
+
+// 转换字符串，undefined,null等转化为""
+export function parseStrEmpty(str) {
+  if (!str || str == "undefined" || str == "null") {
+    return "";
+  }
+  return str;
+}
